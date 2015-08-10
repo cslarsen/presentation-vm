bbe6211d3537abe4b96bb875933e11a7e1652aa7
Christian Stigen Larsen <csl@csl.name>
2015-08-10 21:58:22 +0200

    First commit

---
+++ brainfuck.py
@@ -0,0 +1,40 @@
+import sys
+
+pointer = 0
+memory = [0] * 30000
+program = open(sys.argv[1], "rb")
+
+while True:
+    instruction = program.read(1)
+
+    if instruction == "":
+        break # program is finished
+    if instruction == ">":
+        pointer += 1
+        if pointer > len(memory):
+            pointer = 0
+    elif instruction == "<":
+        pointer -= 1
+        if pointer < 0:
+            pointer = len(memory)-1
+    elif instruction == "+":
+        memory[pointer] += 1
+    elif instruction == "-":
+        memory[pointer] -= 1
+    elif instruction == ".":
+        sys.stdout.write(chr(memory[pointer]))
+        sys.stdout.flush()
+    elif instruction == ",":
+        memory[pointer] = ord(sys.stdin.read(1))
+    elif instruction == "[":
+        if memory[pointer] == 0:
+            while program.read(1) != "]":
+                pass
+            program.seek(-1)
+    elif instruction == "]":
+        continue
+    else:
+        # Skip unknown instructions. 
+        # This makes it possible to write
+        # comments in the code!
+        continue
---
+++ hello.bf
@@ -0,0 +1,40 @@
+# Store the number 72 in the first memory cell
+# The value 72 corresponds to ASCII value of "H"
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++
+
+# Print it
+.
+
+# Instead of decrementing the same cell 72 times
+# an "optimization" is to advance to a fresh cell
+# containing zero and put the number 72 there
+# (the character "E")
+
+> # Advance to next cell
+# Set the number 72
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++++++++++
+++++++++++
++++++++++
+. # Print
+
+# Print "L" twice
+>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++..
+
+# Print "O"
+>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++.
+
+# Print a newline (ASCII value 10)
+>++++++++++.
