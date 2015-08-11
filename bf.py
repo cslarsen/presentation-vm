be8312e234819822ab10ab028a79a63f4f25763d
Christian Stigen Larsen <csl@csl.name>
2015-08-11 16:53:59 +0200

    Adds more

--- bf.py
+++ bf.py
@@ -10,6 +10,8 @@ class Machine(object):
         self.memory = [0]*memory
         self.mptr = 0
 
+        self.silent = True
+
     @property
     def byte(self):
         return self.memory[self.mptr]
@@ -18,13 +20,17 @@ class Machine(object):
     def byte(self, value):
         self.memory[self.mptr] = value
 
-    @property
-    def instruction(self):
-        return self.code[self.cptr]
-
     def next(self):
-        i = self.instruction
+        if self.cptr >= len(self.code):
+            raise StopIteration()
+
+        i = self.code[self.cptr]
         self.cptr += 1
+
+        if not self.silent:
+            sys.stdout.write("%s" % i)
+            sys.stdout.flush()
+
         return i
 
     def step(self):
@@ -39,21 +45,22 @@ class Machine(object):
         elif i == "+":
             self.byte += 1
         elif i == "-":
-            self.byte += 1
+            self.byte -= 1
         elif i == ".":
             sys.stdout.write(chr(self.byte))
             sys.stdout.flush()
         elif i == ",":
-            print("INPUT")
             sys.stdout.flush()
             self.byte = ord(sys.stdin.read(1))
         elif i == "[":
-            self.stack.append(self.cptr)
+            self.stack.append(self.cptr-1)
             if self.byte == 0:
                 self.skip_block()
                 self.stack.pop()
         elif i == "]":
             self.cptr = self.stack[-1]
+        else:
+            pass
 
     def skip_block(self):
         count = 1
@@ -65,15 +72,14 @@ class Machine(object):
                 count -= 1
 
     def run(self):
-        while True:
-            self.step()
+        try:
+            while True:
+                self.step()
+        except StopIteration:
+            pass
 
 if __name__ == "__main__":
     for f in sys.argv[1:]:
         with open(f, "rb") as file:
-            print(f)
-            try:
-                m = Machine(list(file.read()))
-                m.run()
-            except IndexError:
-                pass
+            m = Machine(list(file.read()))
+            m.run()
