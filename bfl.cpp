43947c0850a604fbdd5f63ed375b5e97b6242f8d
Christian Stigen Larsen <csl@csl.name>
2015-08-16 09:00:35 +0200

    More

--- bfl.cpp
+++ bfl.cpp
@@ -6,13 +6,16 @@ extern "C" {
 }
 
 static jit_state_t *_jit;
+typedef void (*vfptr)(void);
 
 struct Program {
   uint8_t *memory;
+  vfptr code;
 
   Program(const size_t memsize = 100000)
     : memory(new uint8_t[memsize])
   {
+    memset(&memory[0], 0, sizeof(uint8_t)*memsize);
   }
 
   ~Program()
@@ -22,6 +25,15 @@ struct Program {
 
   void compile(FILE *f)
   {
+    jit_prolog();
+
+    // Registers:
+    // R0 -- base pointer to memory
+    jit_movi(JIT_R0, 0);
+
+    // R1 -- offset pointer to memory
+ //   jit_movi(JIT_R1, 0);
+
     for ( int c=0; c != EOF; c = fgetc(f) ) {
       switch ( c ) {
         case '<':
@@ -40,15 +52,22 @@ struct Program {
           break;
       }
     }
+
+    code = reinterpret_cast<vfptr>(jit_emit());
   }
 
   void run()
   {
+    printf("running\n");
+    code();
   }
 };
 
 int main(int argc, char *argv[])
 {
+  init_jit(argv[0]);
+  _jit = jit_new_state();
+
   for ( size_t n=1; n<argc; ++n ) {
     if ( argv[n][0] == '-' )
       continue;
@@ -61,5 +80,8 @@ int main(int argc, char *argv[])
     }
   }
 
+  jit_clear_state();
+  jit_destroy_state();
+  finish_jit();
   return 0;
 }
