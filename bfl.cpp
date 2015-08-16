82305759b92485a8fee3e2817afbaf50e3bf023f
Christian Stigen Larsen <csl@csl.name>
2015-08-16 12:37:52 +0200

    More

--- bfl.cpp
+++ bfl.cpp
@@ -8,77 +8,79 @@ extern "C" {
 static jit_state_t *_jit;
 typedef void (*vfptr)(void);
 
-struct Program {
-  uint8_t *memory;
-  vfptr code;
+jit_pointer_t compile(FILE *f, uint8_t *memory)
+{
+  jit_prolog();
+  jit_movi(JIT_V0, reinterpret_cast<jit_word_t>(memory)); // base
+  jit_movi(JIT_V1, 0); // offset
 
-  Program(const size_t memsize = 100000)
-    : memory(static_cast<uint8_t*>(malloc(sizeof(uint8_t)*memsize)))
+  for ( int c=0; c != EOF; c = fgetc(f) ) {
+    switch ( c ) {
+      case '<':
+        jit_addi(JIT_V1, JIT_V1, -1);
+        break;
+      case '>':
+        jit_addi(JIT_V1, JIT_V1, 1);
+        break;
+      case '+':
+        jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
+        jit_addi(JIT_V2, JIT_V2, 1);
+        jit_stxr(JIT_V0, JIT_V1, JIT_V2);
+        break;
+      case '-':
+        jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
+        jit_addi(JIT_V2, JIT_V2, -1);
+        jit_stxr(JIT_V0, JIT_V1, JIT_V2);
+        break;
+      case '.':
+        jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
+        jit_prepare();
+        jit_pushargr(JIT_V2);
+        jit_finishi(reinterpret_cast<jit_pointer_t>(putchar));
+        break;
+      case ',':
+        jit_prepare();
+        jit_finishi(reinterpret_cast<jit_pointer_t>(getchar));
+        jit_retval(JIT_V2);
+        jit_stxr(JIT_V0, JIT_V1, JIT_V2);
+        break;
+      case '[':
+        break;
+      case ']':
+        break;
+      default:
+        break;
+    }
+  }
+
+  jit_epilog();
+  return jit_emit();
+}
+
+struct Machine {
+  uint8_t *memory;
+  vfptr program;
+
+  Machine(const size_t memsize = 100000)
+    : memory(static_cast<uint8_t*>(malloc(sizeof(uint8_t)*memsize))),
+      program(NULL)
   {
     memset(memory, 0, sizeof(uint8_t)*memsize);
   }
 
-  ~Program()
+  void compile(FILE* f)
+  {
+    program = reinterpret_cast<vfptr>(::compile(f, memory));
+  }
+
+  ~Machine()
   {
     free(memory);
   }
 
-  void compile(FILE *f)
-  {
-    jit_prolog();
-
-    // V0 -- base pointer to memory
-    jit_movi(JIT_V0, reinterpret_cast<jit_word_t>(memory));
-
-    // V1 -- offset pointer to memory
-    jit_movi(JIT_V1, 0);
-
-    for ( int c=fgetc(f); c != EOF; c = fgetc(f) ) {
-      switch ( c ) {
-        case '<':
-          jit_addi(JIT_V1, JIT_V1, -1);
-          break;
-        case '>':
-          jit_addi(JIT_V1, JIT_V1, 1);
-          break;
-        case '+':
-          jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
-          jit_addi(JIT_V2, JIT_V2, 1);
-          jit_stxr(JIT_V0, JIT_V1, JIT_V2);
-          break;
-        case '-':
-          jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
-          jit_addi(JIT_V2, JIT_V2, -1);
-          jit_stxr(JIT_V0, JIT_V1, JIT_V2);
-          break;
-        case '.':
-          jit_ldxr(JIT_V2, JIT_V0, JIT_V1);
-          jit_prepare();
-          jit_pushargr(JIT_V2);
-          jit_finishi(reinterpret_cast<jit_pointer_t>(putchar));
-          break;
-        case ',':
-          jit_prepare();
-          jit_finishi(reinterpret_cast<jit_pointer_t>(getchar));
-          jit_retval(JIT_V2);
-          jit_stxr(JIT_V0, JIT_V1, JIT_V2);
-          break;
-        case '[':
-          break;
-        case ']':
-          break;
-        default:
-          break;
-      }
-    }
-
-    jit_epilog();
-    code = reinterpret_cast<vfptr>(jit_emit());
-  }
-
   void run()
   {
-    code();
+    program();
   }
 };
 
@@ -92,7 +94,7 @@ int main(int argc, char *argv[])
       continue;
     FILE *f = fopen(argv[n], "rt");
     if ( f ) {
-      Program p;
+      Machine p;
       p.compile(f);
       fclose(f);
       p.run();
